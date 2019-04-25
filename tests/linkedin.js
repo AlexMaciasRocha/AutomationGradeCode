58900eafc993adffa5904a6608bb8f7f8e7599c8
Alex Macias <alejandro.macias@unosquare.com>
2019-04-25 15:49:38 -0500

    Code with set value error

--- tests/linkedin.js
+++ tests/linkedin.js
@@ -4,8 +4,16 @@ module.exports = {
          browser 
          .url('https://www.linkedin.com/') // Go to a url
          .waitForElementVisible('body', 1000) // wait till page loads
-         //.click('button[name=btnG]') // click on search box
-         .pause(1000)
+         .assert.containsText('h3[class=subtitle]', "Get started - it's free.")
+         .click('input[id=registration-submit]')
+         .assert.containsText('span[class=alert-content]', "Please enter your first name")
+         .setValue('input[id=reg-firstname]','Alejandro')
+         .click('input[id=registration-submit]')
+         .assert.containsText('span[class=alert-content]', "Please enter your last name")
+         .setValue('input[id=reg-lastname]','Macias')
+         .click('input[id=registration-submit]')
+         .assert.containsText('span[class=alert-content]', "Please enter your email address")
+         .pause(10000)
          .end();
        }
  };
