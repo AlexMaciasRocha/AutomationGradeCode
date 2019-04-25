11d80b97a398ff1e50eb12a98ffb5f7d63bcd8aa
Alex Macias <alejandro.macias@unosquare.com>
2019-04-25 17:05:26 -0500

    Initial Commit

--- tests/MS.js
+++ tests/MS.js
@@ -1,10 +1,16 @@
 module.exports = {
-    tags: ['linkedin'],
+    tags: ['ms'],
     'Test LinkedIn' : function (browser) {
          browser 
-         .url('https://www.linkedin.com/') // Go to a url
+         .url('https://www.microsoft.com/en-us/') // Go to a url
          .waitForElementVisible('body', 1000) // wait till page loads
-         .assert.containsText('h3[class=subtitle]', "Get started - it's free.")
+         .pause(1000)
+         .end();
+       }
+ };
+ // = comment not necessary for codecls
+
+ /*.assert.containsText('h3[class=subtitle]', "Get started - it's free.")
          .click('input[id=registration-submit]')
          .assert.containsText('span[class=alert-content]', "Please enter your first name")
          .waitForElementVisible('body', 1000) // wait till page loads
@@ -15,8 +21,5 @@ module.exports = {
          .setValue('input[id=reg-lastname]', 'Macias')
          .click('input[id=registration-submit]')
          .assert.containsText('span[class=alert-content]', "Please enter your email address")
-         .pause(10000)
-         .end();
-       }
- };
- // = comment not necessary for codecls
+         
+         */
