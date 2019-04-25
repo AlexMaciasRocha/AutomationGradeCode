a69509b5cf75e7aa79127099f8ccce252cd8177a
Alex Macias <alejandro.macias@unosquare.com>
2019-04-25 17:43:39 -0500

    till bullet 5

--- tests/MS.js
+++ tests/MS.js
@@ -4,7 +4,18 @@ module.exports = {
          browser 
          .url('https://www.microsoft.com/en-us/') // Go to a url
          .waitForElementVisible('body', 1000) // wait till page loads
-         .pause(1000)
+         .click('a[id=shellmenu_1]')
+         .click('button[id=c-shellmenu_42]')
+         .waitForElementVisible('body', 1000)
+         .assert.containsText('a[id=c-shellmenu_43]', "Buy Windows 10 Home")
+         .assert.containsText('a[id=c-shellmenu_45]', "Why Windows 10")
+         .assert.containsText('a[id=c-shellmenu_46]', "Features")
+         .click('a[id=c-shellmenu_43]')
+         .waitForElementVisible('body', 1000) // wait till page loads
+         .click('a[id=c-shellmenu_43]')
+         //.click('div[class=c-glyph glyph-cancel]')
+         //.assert.containsText('a[id=titleGroup]', "Windows 10 Home")
+         .pause(1000000)
          .end();
        }
  };
