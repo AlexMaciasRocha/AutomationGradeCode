3696d2cd374857cab8a2e82e23b0e39d1de8ad85
Alex Macias <alejandro.macias@unosquare.com>
2019-04-29 12:23:25 -0500

    Submit to grade

--- tests/MS.js
+++ tests/MS.js
@@ -12,25 +12,19 @@ module.exports = {
          .assert.containsText('a[id=c-shellmenu_46]', "Features")
          .click('a[id=c-shellmenu_43]')
          .waitForElementVisible('body', 1000) // wait till page loads
-         .click('a[id=c-shellmenu_43]')
-         //.click('div[class=c-glyph glyph-cancel]')
-         //.assert.containsText('a[id=titleGroup]', "Windows 10 Home")
+         .click('div[class="c-glyph glyph-cancel"')
+         .waitForElementVisible('body', 1000)
+         .assert.containsText('p[class="Paragraph SCX256709826"]', "¿Buscas características empresariales?")
+         .click('form[id=searchForm]')
+         .setValue('input[id=cli_shellHeaderSearchInput]', 'Visual Studio Community')
+         .click('button[id=search]')
+         .saveScreenshot('./reports/SearchEvidence.png')
+         .assert.containsText('div[data-grid=col-2]', "1-20 de ")
+         .click('li[data-label="Página 2"]')
+         .assert.containsText('div[data-grid=col-2]', "21-40 de ")
+         .click('li[data-label="Página 3"]')
+         .assert.containsText('div[data-grid=col-2]', "41-60 de ")
          .pause(1000000)
          .end();
        }
  };
- // = comment not necessary for codecls
-
- /*.assert.containsText('h3[class=subtitle]', "Get started - it's free.")
-         .click('input[id=registration-submit]')
-         .assert.containsText('span[class=alert-content]', "Please enter your first name")
-         .waitForElementVisible('body', 1000) // wait till page loads
-         .click('input[id=reg-firstname]')
-         .setValue('input[id=reg-firstname]', 'Alejandro')
-         .click('input[id=registration-submit]')
-         .assert.containsText('span[class=alert-content]', "Please enter your last name")
-         .setValue('input[id=reg-lastname]', 'Macias')
-         .click('input[id=registration-submit]')
-         .assert.containsText('span[class=alert-content]', "Please enter your email address")
-         
-         */
