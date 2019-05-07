d088e4f07ae789f3b830dc42de986c32af8c76e8
Alex Macias <alejandro.macias@unosquare.com>
2019-05-07 14:30:57 -0500

    Till step 7

--- tests/YouTube.js
+++ tests/YouTube.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
     tags: ['youtube'],
     before : function(browser) {
       console.log('Setting up...');
@@ -26,4 +26,30 @@ module.exports = {
       // ...
         .end();
     }
-  };
+  };*/
+
+  module.exports = {
+    tags: ['youtube'],
+    'Test Youtube' : function (browser) {
+         browser.maximizeWindow()
+         .url('https://www.youtube.com/') // Go to a url
+         .waitForElementVisible('body', 5000) // wait till page loads
+         .click('yt-icon-button[id="guide-button"]')
+         .waitForElementVisible('body', 5000)
+         .assert.containsText('a[title="Home"]', "Home")
+         .waitForElementVisible('body', 5000)
+         .assert.containsText('a[title="Trending"]', "Trending")
+         .waitForElementVisible('body', 5000)
+         .assert.containsText('a[title="History"]', "History")
+         .waitForElementVisible('body', 5000)
+         .assert.containsText('paper-button[aria-label="Sign in"]', "SIGN IN")
+         .click('button[aria-label="YouTube apps"]')
+         .click('form[id=search-form]')
+         .setValue('input[id="search"]', 'Automating with Node.js – 001')
+         .click('button[class="style-scope ytd-searchbox"]')
+         .pause(1000000)
+         .end();
+   
+        }
+ };
+
